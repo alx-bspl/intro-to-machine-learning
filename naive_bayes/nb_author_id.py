3dcc43602e7577fe79a3d923956dd4866b6dd59b
Oleksii Bespalov <alexb.5dh@gmail.com>
2019-12-12 01:40:59 +0200

    Implemented Naive Bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,25 +9,32 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+clf = GaussianNB()
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print(f"Training time: {round(time()-t0, 3)}")  # ~2.6s
+
+t0 = time()
+labels_pred = clf.predict(features_test)
+print(f"Prediction time: {round(time()-t0, 3)}")  # 0.5s
+
+acc = accuracy_score(labels_test, labels_pred)
+print(f"Accuracy: {acc}")  # ~0.973
 
 #########################################################
-
-
