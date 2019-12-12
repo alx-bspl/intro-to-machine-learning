58429a89634b1f528aa670993ca4b7f033bd2957
Oleksii Bespalov <alexb.5dh@gmail.com>
2019-12-12 21:00:45 +0200

    Implemented Support Vectors

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,19 +12,28 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+clf = SVC(kernel='linear')
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print(f"Training time: {round(time()-t0, 3)}")  # ~388s
+
+t0 = time()
+labels_pred = clf.predict(features_test)
+print(f"Prediction time: {round(time()-t0, 3)}")  # ~47s
+
+acc = accuracy_score(labels_test, labels_pred)
+print(f"Accuracy: {acc}")  # ~0.984
 #########################################################
 
 
