7ce995f52b951c1c47c32302cf092be245b5e3fb
Oleksii Bespalov <alexb.5dh@gmail.com>
2019-12-12 21:35:53 +0200

    Logging improvements

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -23,6 +23,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 clf = LinearSVC()
+print(f"Using {clf}")
 
 t0 = time()
 clf.fit(features_train, labels_train)
